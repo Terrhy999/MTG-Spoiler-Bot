55415af2e7f95c1c6e46115aeac5c895c1203d0d
Terrhy999 <thyla999@gmail.com>
2024-03-28 15:33:55 -0400

    create discord.js bot

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import Discord = require("discord.js");
+import "dotenv/config";
+
+const startBot = () => {
+  const client = new Discord.Client({
+    intents: ["Guilds", "GuildMessages", "MessageContent"],
+  });
+
+  const prefix = "!mtg";
+
+  client.on("messageCreate", async function (message) {
+    console.log(message.content);
+
+    if (message.author.bot) {
+      return;
+    }
+    if (!message.content.toLowerCase().startsWith(prefix)) {
+      return;
+    }
+
+    const commandBody = message.content.slice(prefix.length).trim();
+
+    if (!commandBody) {
+      console.log("empty command");
+      await message.channel.send(
+        "Please provide a 3-letter set code as an argument for the command.",
+      );
+      return;
+    }
+
+    const args = commandBody.split(/\s+/);
+
+    const command = args.shift()?.toLowerCase();
+
+    await message.reply(`Now tracking spoilers for set ${command}`);
+  });
+
+  void client.login(process.env["BOT_TOKEN"]);
+};
+
+startBot();
